5a3aff40a53114ee79ea71cb59ebc13eb01804c7
Erich Engler <erich.engler@hotmail.com>
2023-01-18 20:54:53 -0600

    finished group work

--- client.js
+++ client.js
@@ -14,6 +14,15 @@
 // 3. painting
 // Total hobbies: 3
 
+let groupHobbies = [ 'cycling', 'gaming', 'lifting', 'coding' ];
+let listNumber = 1
+for ( let element of groupHobbies ) {
+    console.log( `${listNumber}. ${element}` );
+    listNumber += 1;
+}
+console.log( `Total hobbies: ${groupHobbies.length}` );
+
+
 /**
  * #2 Colors
  * ---------
@@ -30,6 +39,18 @@
 // green, red, teal, orange, teal
 // Teal was found 2 times
 
+let colors = [ 'orange', 'red', 'blue', 'teal' ];
+let tealCount = 0;
+for ( i=0; i<colors.length; i++ ) {
+    if ( colors[i] === 'teal' ) {
+        tealCount += 1
+    }
+}
+console.log( colors );
+if ( tealCount = 1 ) {
+    console.log( 'Teal was found ' + tealCount + ' time.' )
+} else
+console.log( 'Teal was found ' + tealCount + ' times.' )
 
 /**
  * #3 Even & Odd
@@ -47,6 +68,19 @@
 // Odd 3, 7, 11
 // Even 2, 8, 4, 2
 
+let numbers = [ 1, 3, 6, 8, 10 ];
+let oddNumbers = [];
+let evenNumbers = [];
+for ( i = 0; i < numbers.length; i++ ) {
+    if ( numbers[i] % 2 === 0 ) {
+        evenNumbers.push( numbers[i] );
+    } else {
+        oddNumbers.push( numbers[i] );
+    }
+} console.log( numbers );
+console.log( 'The even numbers are: ' + evenNumbers );
+console.log( 'The odd numbers are: ' + oddNumbers );
+
 
 /**
  * #4 Flipping Switches
@@ -62,6 +96,18 @@
 // true, false, true, true
 // Toggled false, true, false, false
 
+let booleanArray = [ true, false, false, true, true ];
+let toggled = [];
+for ( let boolean of booleanArray ) {
+    if ( boolean === true ) {
+        toggled.push( false );
+    } else {
+        toggled.push( true );
+    }
+}
+console.log( booleanArray );
+console.log ( toggled );
+
 
 /**
  * #5 Remove 0's
@@ -74,6 +120,14 @@
  */
 
 
+
 // Example output
 // Before loop 3, 0, 2, 8, 0, 0, 0
 // After loop 3, 0, 2, 8
+
+let numberList = [ 5, 6, 0, 7, 0, 0 ];
+for ( let element of numberList ) {
+    if ( numberList[numberList.length - 1] === 0 ) {
+        numberList.pop();
+    }
+} console.log( numberList );
